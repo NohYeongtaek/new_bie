442bea690b630b3f79ce71055ad511ecd1ff1c6c
kmj685 <kmj202005@naver.com>
2025-11-04 22:30:32 +0900

    feat empty view model, method channel image picker

--- android/app/src/main/kotlin/com/newBie/new_bie/MainActivity.kt
+++ android/app/src/main/kotlin/com/newBie/new_bie/MainActivity.kt
@@ -1,5 +1,45 @@
 package com.newBie.new_bie
 
+import android.util.Log
+import androidx.core.net.toFile
+import gun0912.tedimagepicker.builder.TedImagePicker
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.io.File
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger,
+            "com.myApp.app/image")
+            .setMethodCallHandler { call, result ->
+                if (call.method =="getImageUri") {
+                    showImagePicker(result)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+    private fun showImagePicker(result: MethodChannel.Result) {
+
+        TedImagePicker.with(this)
+            .start { uri ->
+                val file = if (uri.scheme =="content") {
+                    val inputStream = contentResolver.openInputStream(uri)
+                    val tempFile = File(cacheDir,"temp_image_${System.currentTimeMillis()}.png")
+                    inputStream?.use { input ->
+                        tempFile.outputStream().use { output  ->
+                            input.copyTo(output)
+                        }
+                    }
+                    tempFile
+                } else {
+                    uri.toFile()
+                }
+                Log.d("이미지 선택 uri", "showImagePicker - uri: $uri")
+                val imageUri = uri.toString()
+                result.success(file.path)
+            }
+    }
+}
---
+++ lib/src/source_code_blocks/view_model/empty_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class EmptyViewModel extends ChangeNotifier {
+  // int inputCount = 0;
+
+  // final TextEditingController textEditingController = TextEditingController();
+
+  // 뷰모델 생성자, context를 통해 리포지토리를 받아올 수 있음.
+  EmptyViewModel(BuildContext context) {}
+
+  // 입력한 글자 수를 받아오는 함수
+  // void handleTextInput(String input) {
+  //   inputCount = input.length;
+  //   notifyListeners();
+  // }
+
+  // Future 함수는 API나 Supabase, 메소드 채널과 사용할 때,
+  // 처리를 하는데 시간이 걸리는 작업을 할때 사용됨.
+  // 아래 함수 안 에서는 시간이 필요한 작업은 await를 사용 해야함
+  // Future<void> function() async {
+  //   try {
+  //
+  //   } catch (e) {
+  //
+  //   }
+  //
+  //   // 리빌딩, 리콤포지션 진행
+  //   notifyListeners();
+  // }
+  // Future<void> function1() async {
+  //   // 시간 지연 코드
+  //   await Future.delayed(const Duration(milliseconds: 1700));
+  //   notifyListeners();
+  // }
+}
