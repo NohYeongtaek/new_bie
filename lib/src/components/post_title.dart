ec988498493f399676cc3e775d8a170d251fd7b8
blackdot0525-stack <blackdot0525@gmail.com>
2025-11-05 15:51:09 +0900

    포스트 타이틀 컴포넌트입니다

--- lib/src/components/post_title.dart
+++ lib/src/components/post_title.dart
@@ -14,6 +14,7 @@ class PostTitle extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.fromLTRB(12, 4, 12, 4), // 내부여백 4
         decoration: BoxDecoration( // 박스 스타일
+          //color: Colors.blue, // 박스모양 볼때
           borderRadius: BorderRadius.circular(8), // 모서리
         ),
         child: Text(
