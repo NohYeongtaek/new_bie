6ab7640d6b1e1aed0ed0d90c8c816f31bfa35aca
nohyeongteak <yt873588@gmail.com>
2025-11-05 14:28:15 +0900

    테스트

---
+++ lib/src/source_code_blocks/ui_set/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+// 배경색
+const Color orangeColor = Color(0xff101820);
+// 글씨색
+const Color blackColor = Color(0xffF2AA4C);
+// 본문 글씨색은 하얀색으로
---
+++ lib/src/source_code_blocks/ui_set/fonts.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+final TextStyle titleFontStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+final TextStyle contentFontStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+);
+final TextStyle pageTitleFontStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+final TextStyle dateFontStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+);
+final TextStyle normalFontStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+);
+final TextStyle buttonFontStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+final TextStyle countFontStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+);
