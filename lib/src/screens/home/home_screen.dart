6951102366abc1589f7e7e0d29d18a17fb246725
kmj685 <kmj202005@naver.com>
2025-11-05 12:26:58 +0900

    ui_set을 assets에서 src 폴더로 옮겼습니다.홈에서 TextStyle 적용해봤습니다.

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../ui_set/fonts.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -7,7 +9,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("임시화면")),
-      body: SafeArea(child: Column(children: [Text("임시화면입니다.")])),
+      body: SafeArea(
+        child: Column(
+          children: [Text("임시화면입니다.", style: contentFontStyle as TextStyle)],
+        ),
+      ),
     );
   }
 }
--- lib/src/ui_set/fonts.dart
+++ lib/src/ui_set/fonts.dart
@@ -1,30 +1,32 @@
 import 'dart:ui';
 
-final TextStyle titleFontStyle = TextStyle(
+TextStyle titleFontStyle = TextStyle(
+  fontSize: 32, // 40까지도
+  fontWeight: FontWeight.bold,
+);
+TextStyle contentFontStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+);
+TextStyle pageTitleFontStyle = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.bold,
 );
-final TextStyle contentFontStyle = TextStyle(
-  fontSize: 16,
-  fontWeight: FontWeight.normal,
-);
-final TextStyle pageTitleFontStyle = TextStyle(
-  fontSize: 20,
-  fontWeight: FontWeight.bold,
-);
-final TextStyle dateFontStyle = TextStyle(
+TextStyle dateFontStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.normal,
 );
-final TextStyle normalFontStyle = TextStyle(
+TextStyle normalFontStyle = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.normal,
 );
-final TextStyle buttonFontStyle = TextStyle(
+TextStyle buttonFontStyle = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.bold,
 );
-final TextStyle countFontStyle = TextStyle(
+TextStyle countFontStyle = TextStyle(
   fontSize: 14,
   fontWeight: FontWeight.normal,
 );
+
+// 최소 12 이상
