0b1ee4b81b31dea401f1fb9dac20979db8006037
leegundo <dlrjseh117@gmail.com>
2025-11-06 11:20:52 +0900

    수파베이스 메니저: 아래부분에 공지사항 관련 부분 추가했습니다
    노티스 레포지토리 파일 작성

--- lib/src/screens/setting/notice/notices_repository.dart
+++ lib/src/screens/setting/notice/notices_repository.dart
@@ -1,1 +1,8 @@
-class NoticesRepository {}
+import 'package:new_bie/src/entity/notice_entity.dart';
+import 'package:new_bie/src/managers/supabase_manager.dart';
+
+class NoticesRepository {
+  Future<List<NoticeEntity>> fetchNotices() async {
+    return await SupabaseManager.shared.fetchNotices();
+  }
+}
